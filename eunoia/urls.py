ff510e6aef97080240019bb7cb0cfa4a988a7fe2
Zoe Nguyen <nganguyen0703@gmail.com>
2022-10-19 16:38:46 +0800

    chore: add djangorestframework

--- eunoia/urls.py
+++ eunoia/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include
+from rest_framework import routers, serializers, viewsets
+router = routers.DefaultRouter()
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
