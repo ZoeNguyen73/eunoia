26f63940e6ff28127a99bfbdd4b17f2d82a9e0ee
Zoe Nguyen <nganguyen0703@gmail.com>
2022-10-22 15:29:56 +0800

    chore: removed redundant code

--- organizations/views.py
+++ organizations/views.py
@@ -11,21 +11,3 @@ class OrganizationViewSet(ModelViewSet):
   queryset = Organization.objects.filter(status='active')
   lookup_field = 'slug'
   lookup_url_kwarg = 'slug'
-
-  # def get_queryset(self):
-  #   queryset = Organization.objects.filter(status='active')
-  #   # slug = self.kwargs['slug']
-  #   # if slug is not None:
-  #   #   queryset = queryset.filter(slug=slug)
-  #   return queryset
-
-  # def list(self, request):
-  #   queryset = Organization.objects.filter(status='ACTIVE')
-  #   serializer = OrganizationSerializer(queryset, many=True)
-  #   return Response(serializer.data)
-  
-  # def retrieve(self, request, pk=None):
-  #   queryset = Organization.objects.all()
-  #   organization = get_object_or_404(queryset, pk=pk)
-  #   serializer = OrganizationSerializer(organization)
-  #   return Response(serializer.data)
