9c940024d25157521b041f6261d7afa85697c2bc
Zoe Nguyen <nganguyen0703@gmail.com>
2022-10-24 18:16:45 +0800

    chore: remove redundant code

--- users/views.py
+++ users/views.py
@@ -1,8 +1,5 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import AllowAny, IsAuthenticated
-from rest_framework.response import Response
-from rest_framework import status
-from django.shortcuts import redirect
 
 from .models import User
 from .serializers import UserSerializer, UserProfileImageSerializer, UserPasswordSerializer
