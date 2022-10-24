45c5d90b105f55c53a0600a0267075efb694ad0b
Zoe Nguyen <nganguyen0703@gmail.com>
2022-10-24 22:23:20 +0800

    feat: allow superuser to activate and deactivate organization

--- organizations/permissions.py
+++ organizations/permissions.py
@@ -5,3 +5,7 @@ from users.models import User
 class IsOrganizationAdmin(BasePermission):
   def has_object_permission(self, request, view, obj):
     return obj == request.user.organization
+
+class IsSuperUser(BasePermission):
+  def has_object_permission(self, request, view, obj):
+    return request.user.is_superuser
